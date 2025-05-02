3cca7c3cf77dabfc4b2d4d7ee9096dba1d80cd06
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-02 08:49:56 +0500

    Create review.js

---
+++ models/review.js
@@ -0,0 +1,23 @@
+const { number } = require("joi");
+const mongoose = require("mongoose");
+const Schema  = mongoose.Schema;
+
+const reviewSchema = Schema({
+
+    comment: string,
+    rating: {
+
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    created_at: {
+
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
