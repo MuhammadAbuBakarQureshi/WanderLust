2b3d07848790509066af3a70531a467501450db8
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-05 11:42:01 +0500

    Create themes.js

---
+++ public/js/themes.js
@@ -0,0 +1,21 @@
+// let body = document.querySelector("body");
+// let themeButton = document.querySelector(".theme-button");
+
+
+// let darkMode = false;
+
+// themeButton.addEventListener("click", () => {
+
+//     if(darkMode == false){
+
+//         themeButton.innerHTML = "Dark Mode";
+//         body.classList.add("dark-mode");
+        
+//         darkMode = true;
+//     }else{
+
+//         themeButton.innerHTML = "Light Mode";
+//         body.classList.remove("dark-mode");
+//         darkMode = false;
+//     }
+// })
