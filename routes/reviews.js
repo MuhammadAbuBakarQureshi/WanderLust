6f42a28427f1fa7347317e40acc8e7b090acde1c
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-05 11:06:43 +0500

    Create reviews.js

---
+++ routes/reviews.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+
+const ExpressError = require("../utils/ExpressError");
+const wrapAsync = require("../utils/wrapAsync");
+const {reviewSchema} = require("../schema");
+
+
+const validateReview = (req, res, next) => {
+
+    let {error} = reviewSchema.validate(req.body);
+
+    if(error){
+
+        throw new ExpressError(400, error);
+    }else{
+
+        next();
+    }
+};
+
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+
+    console.log(id);
+    console.log(req.body.review);
+
+    let listing = await Listing.findById(id);
+    let review = new Review(req.body.review);
+
+    listing.reviews.push(review);
+
+    review.save();
+    listing.save();
+
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+router.delete("/:reviewId", wrapAsync(async (req, res) => {
+
+  let {id, reviewId} = req.params;
+
+  console.log(id);
+  console.log(reviewId);
+
+  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+
+  res.redirect(`/listings/${id}`);
+}));
+
+module.exports = router;
