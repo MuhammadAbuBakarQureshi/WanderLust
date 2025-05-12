078b43567ed8cbed6993b33cf18f08b53d6e1e7c
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-12 07:31:21 +0500

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -39,6 +39,8 @@ router.post(
     review.save();
     listing.save();
 
+    req.flash("success", "Review added successfully!");
+
     res.redirect(`/listings/${id}`);
   })
 );
@@ -52,6 +54,8 @@ router.delete("/:reviewId", wrapAsync(async (req, res) => {
 
   await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
   await Review.findByIdAndDelete(reviewId);
+
+  req.flash("success", "Review deleted successfully!");
 
   res.redirect(`/listings/${id}`);
 }));
