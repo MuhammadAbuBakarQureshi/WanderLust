794154894a3cd9f8d0384252f25e1245925f5f38
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-13 18:50:03 +0500

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,13 @@
+const isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.flash("error", "You must be logged in to perform this action");
+    return res.redirect("/login");
+  }
+
+  next();
+};
+
+module.exports = {
+
+    isLoggedIn
+}
