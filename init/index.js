626278a508288ee20a52937254363ee79946695d
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-14 19:11:35 +0500

    Update index.js

--- init/index.js
+++ init/index.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 const listingdata = require("./data");
 const Listing = require("../models/listing");
+const { listingSchema } = require("../schema");
 
 const initDB = async  () => {
 
     await Listing.deleteMany({});
-        
+    
+    listingdata.data = listingdata.data.map((obj) => ({
+      ...obj,
+      owner: "68234d72d5553b3eaa969d1e",
+    }));
+
     await Listing.insertMany(listingdata.data)
     .then((res) => {
         console.log("Data stored successful!");
