3ce1ea1dbf5599237eee946a7cc7980393b4c0cb
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-15 12:10:14 +0500

    Update review.js

--- models/review.js
+++ models/review.js
@@ -15,6 +15,11 @@ const reviewSchema = new Schema({
 
         type: Date,
         default: Date.now()
+    },
+    author: {
+
+        type: Schema.Types.ObjectId,
+        ref: "User"
     }
 });
 
