38b977dd8dec4b1279cefb0d06fc165a519c442c
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-15 18:19:13 +0500

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,37 +1,37 @@
 const express = require("express");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-const {saveRedirectUrl} = require("../middleware.js");
+const { saveRedirectUrl } = require("../middleware.js");
 const userControllers = require("../Controllers/users");
-
 
 const router = express.Router();
 
 
 // signup requests
 
-router.get("/signup", userControllers.renderSignupForm);
+router
+  .route("/signup")
+  .get(userControllers.renderSignupForm)
+  .post(wrapAsync(userControllers.signup));
 
-router.post("/signup", wrapAsync(userControllers.signup));
 
 // login requests
 
-router.get("/login", userControllers.renderLoginForm);
+router
+  .route("/login")
+  .get(userControllers.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    wrapAsync(userControllers.login)
+  );
 
-router.post(
-  "/login",
-  saveRedirectUrl,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  wrapAsync(userControllers.login)
-);
-
-
+  
 // logout requests
 
-router.get("/logout", userControllers.logout)
+router.get("/logout", userControllers.logout);
 
 module.exports = router;
-  
