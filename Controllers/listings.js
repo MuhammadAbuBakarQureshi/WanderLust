70f81f61a685a47848561a404f839f8df2672fd8
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-15 21:42:06 +0500

    Update listings.js

--- Controllers/listings.js
+++ Controllers/listings.js
@@ -37,8 +37,14 @@ const showListing = async (req, res) => {
 
 const createListing = async (req, res, next) => {
 
+    let url = req.file.path;
+    let filename = req.file.filename;
+    
+
     let newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = { url, filename };
+
     let savedNewListing = await newListing.save();
 
     req.flash("success", "New Listing is added successfully!");
