df8b6c112b8aaa2e271c73c02542d3df1ffb3163
Muhammad Abu Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2025-06-27 12:15:45 +0500

    Create taxSwitch.js

---
+++ public/js/taxSwitch.js
@@ -0,0 +1,18 @@
+
+let taxSwitch = document.querySelector(".switch");
+
+taxSwitch.addEventListener("click", () => {
+
+    let taxInfo = document.querySelectorAll(".tax-info");
+
+    for(let tax of taxInfo){
+
+        if(tax.style.display != "inline"){
+            
+            tax.style.display = "inline";
+        }else{
+
+            tax.style.display = "none";
+        }
+    }
+})
