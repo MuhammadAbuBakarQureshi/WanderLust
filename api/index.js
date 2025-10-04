cb09b048fe42afde6ea3c56304142379320ae6b8
Muhammad Abu Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2025-10-04 18:01:43 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,30 +1,4 @@
-// api/index.js
-const serverless = require("serverless-http");
-const mongoose = require("mongoose");
-const app = require("../app");
+import app from "../app.js";
+import serverless from "serverless-http";
 
-let isConnected = false;
-
-async function connectDB() {
-  if (isConnected) return;
-  const uri = process.env.ATLASDB_URL;
-
-  if (!uri) {
-    console.error("❌ ATLASDB_URL is missing in env variables!");
-    return;
-  }
-
-  try {
-    await mongoose.connect(uri);
-    isConnected = true;
-    console.log("✅ Connected to MongoDB");
-  } catch (err) {
-    console.error("❌ Error while connecting to MongoDB:", err);
-  }
-}
-
-module.exports = async (req, res) => {
-  await connectDB(); // connect only when needed
-  const handler = serverless(app);
-  return handler(req, res);
-};
+export const handler = serverless(app);
