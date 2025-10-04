08f697f469d5c14375d4611ed057ae6386b2693a
Muhammad Abu Bakar Qureshi <qureshimuhammadabubakar@gmail.com>
2025-10-04 18:14:57 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,6 @@
 import app from "../app.js";
-import mongoose from "mongoose";
 import serverless from "serverless-http";
 
-const connectDB = async () => {
-  try {
-    const mongoUrl = process.env.ATLASDB_URL;
-    await mongoose.connect(mongoUrl);
-    console.log("✅ MongoDB connected successfully");
-  } catch (err) {
-    console.error("❌ Error while connecting to MongoDB:", err);
-  }
-};
-
-connectDB();
 
 export const handler = serverless(app);
 export default handler;
